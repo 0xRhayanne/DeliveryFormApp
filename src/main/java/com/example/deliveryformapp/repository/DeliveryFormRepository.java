e3a612ac719102e03103ac2bcf0c4ef06ed2a5e3
Rhayanne <rhay.naka@gmail.com>
2026-04-01 18:01:01 -0300

    Update

--- src/main/java/com/example/deliveryformapp/repository/DeliveryFormRepository.java
+++ src/main/java/com/example/deliveryformapp/repository/DeliveryFormRepository.java
@@ -4,6 +4,4 @@ import com.example.deliveryformapp.model.DeliveryForm;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface DeliveryFormRepository extends MongoRepository<DeliveryForm, String> {
-    // You can add custom queries later, e.g.:
-    // List<DeliveryForm> findByStatus(String status);
 }
